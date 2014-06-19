41b2998ea4a662596226acf2ee861ed2649d5e13
ana-balica <ana.balica@gmail.com>
2014-06-19 17:36:55 +0300

    add and configure django-nose

--- systers_portal/systers_portal/settings/base.py
+++ systers_portal/systers_portal/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -68,3 +69,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
