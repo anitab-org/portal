45735143a1aff11184646a41a37013cc0a5f9ded
ana-balica <ana.balica@gmail.com>
2014-10-07 00:23:34 +0300

    add custom permission to Community model

--- systers_portal/community/models.py
+++ systers_portal/community/models.py
@@ -30,6 +30,24 @@ class Community(models.Model):
 
     class Meta:
         verbose_name_plural = "Communities"
+        permissions = (
+            ('add_community_systersuser', 'Add community Systers User'),
+            ('change_community_systersuser', 'Change community Systers User'),
+            ('delete_community_systersuser', 'Delete community Systers User'),
+            ('add_community_news', 'Add community news'),
+            ('change_community_news', 'Change community news'),
+            ('delete_community_news', 'Delete community news'),
+            ('add_community_resource', 'Add community resource'),
+            ('change_community_resource', 'Change community resource'),
+            ('delete_community_resource', 'Delete community resource'),
+            ('add_community_page', 'Add community page'),
+            ('change_community_page', 'Change community page'),
+            ('delete_community_page', 'Delete community page'),
+            ('approve_community_comment', 'Approve community comment'),
+            ('delete_community_comment', 'Delete community comment'),
+            ('approve_community_joinrequest', 'Approve community join '
+                                              'request'),
+        )
 
     def __unicode__(self):
         return self.name
