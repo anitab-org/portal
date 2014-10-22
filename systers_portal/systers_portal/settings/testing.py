5e18c9f114c4e88aacf215476e4cee5b11bbb2e2
ana-balica <ana.balica@gmail.com>
2014-10-23 01:06:31 +0800

    update travis.yml to run tests using testing settings

--- systers_portal/systers_portal/settings/testing.py
+++ systers_portal/systers_portal/settings/testing.py
@@ -6,7 +6,7 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
+        'NAME': 'systersdb',
         'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost',
