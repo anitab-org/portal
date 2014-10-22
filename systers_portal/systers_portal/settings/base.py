b97f0c3b17932de36559210aa8137dd3ada6865c
ana-balica <ana.balica@gmail.com>
2014-10-22 16:00:34 +0800

    add static files dirs to settings

--- systers_portal/systers_portal/settings/base.py
+++ systers_portal/systers_portal/settings/base.py
@@ -105,9 +105,11 @@ SITE_ID = 1
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
 
-STATIC_URL = "/static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
