3d80e7ae7c4f37891bbf8eff2aa96add3d63410e
ana-balica <ana.balica@gmail.com>
2014-10-29 23:00:34 +0800

    refactor UserView tests and add tests for profile and membership panels

--- systers_portal/users/tests.py
+++ systers_portal/users/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User, Group
 from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
 
+from community.models import Community, JoinRequest
 from users.models import SystersUser
 
 
@@ -42,27 +43,70 @@ class SystersUserViewsTestCase(TestCase):
         self.systers_user = SystersUser.objects.get()
         self.client = Client()
 
-    def test_user_profile_view(self):
-        """Test UserProfileView as a logged-in user"""
-        self.client.login(username='foo', password='foobar')
-        profile_url = reverse('user_profile', kwargs={
+    def test_user_view(self):
+        """Test UserView as a logged-in user"""
+        user_url = reverse('user', kwargs={
             'username': self.systers_user.user.username})
-        response = self.client.get(profile_url)
+        response = self.client.get(user_url)
+        self.assertEqual(response.status_code, 302)
+        self.client.login(username='foo', password='foobar')
+        response = self.client.get(user_url)
         self.assertEqual(response.status_code, 200)
+        nonexistent_user_url = reverse('user', kwargs={'username': 'bar'})
+        response = self.client.get(nonexistent_user_url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_user_profile_panel(self):
+        """Test profile panel from UserView"""
+        self.client.login(username='foo', password='foobar')
+        user_url = reverse('user', kwargs={
+            'username': self.systers_user.user.username})
+        response = self.client.get(user_url)
         self.assertContains(response, 'Edit profile')
         self.assertTemplateUsed(response, 'users/view_profile.html')
         self.assertTemplateUsed(response, 'users/snippets/profile.html')
-        nonexisten_profile_url = reverse('user_profile', kwargs={
-            'username': 'bar'})
-        response = self.client.get(nonexisten_profile_url)
-        self.assertEqual(response.status_code, 404)
 
         new_user = User.objects.create_user(username='bar', password='foobar')
-        new_user_profile_url = reverse('user_profile', kwargs={
-            'username': new_user.username})
-        response = self.client.get(new_user_profile_url)
+        new_user_url = reverse('user', kwargs={'username': new_user.username})
+        response = self.client.get(new_user_url)
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, 'Edit profile')
+
+    def test_user_panel_membership_view(self):
+        """Test membership panel from UserView"""
+        self.client.login(username='foo', password='foobar')
+        user_url = reverse('user', kwargs={
+            'username': self.systers_user.user.username})
+        response = self.client.get(user_url)
+        self.assertContains(response,
+                            "Looks like you are member of no community.")
+        self.assertTemplateUsed(response, 'users/view_profile.html')
+        self.assertTemplateUsed(response, 'users/snippets/membership.html')
+
+        community = Community.objects.create(name="Foo", slug="foo", order=1,
+                                             community_admin=self.systers_user)
+        community.add_member(self.systers_user)
+        community.save()
+        response = self.client.get(user_url)
+        self.assertContains(response, "Transfer ownership")
+        new_user = User.objects.create_user(username='bar', password='foobar')
+        new_systers_user = SystersUser.objects.get(user=new_user)
+        new_comm = Community.objects.create(name="Bar", slug="bar",
+                                            order=2,
+                                            community_admin=new_systers_user)
+        new_comm.add_member(self.systers_user)
+        new_comm.save()
+        response = self.client.get(user_url)
+        self.assertContains(response, "Leave")
+        new_comm.remove_member(self.systers_user)
+        new_comm.save()
+        JoinRequest.objects.create(user=self.systers_user, community=new_comm)
+        response = self.client.get(user_url)
+        self.assertContains(response, "Cancel request")
+
+        new_user_url = reverse('user', kwargs={'username': new_user.username})
+        response = self.client.get(new_user_url)
+        self.assertNotContains(response, 'Transfer ownership')
 
 
 class UserTestCase(TestCase):
