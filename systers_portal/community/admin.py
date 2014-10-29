29339b3f52364701353bb79dbac06d6c56f8a656
ana-balica <ana.balica@gmail.com>
2014-10-29 20:44:46 +0800

    add community_admin to community members via admin panel

--- systers_portal/community/admin.py
+++ systers_portal/community/admin.py
@@ -5,7 +5,13 @@ from community.models import Community, CommunityPage
 
 
 class CommunityAdmin(GuardedModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        """Override this method in order to be able to add the community admin
+        to members list via admin panel."""
+        members = list(form.cleaned_data['members'])
+        members.extend([obj.community_admin])
+        form.cleaned_data['members'] = members
+        super(CommunityAdmin, self).save_model(request, obj, form, change)
 
 
 admin.site.register(Community, CommunityAdmin)
