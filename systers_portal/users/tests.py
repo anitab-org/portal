5da87ea53435e0392a39b3f8d83b46f076237d99
ana-balica <ana.balica@gmail.com>
2014-11-02 14:34:36 +0800

    add UserProfileView TestCase (unittests)

--- systers_portal/users/tests.py
+++ systers_portal/users/tests.py
@@ -124,6 +124,89 @@ class UserViewTestCase(TestCase):
         self.assertContains(response, "Bar")
 
 
+class UserProfileViewTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='foo', password='foobar')
+        self.systers_user = SystersUser.objects.get()
+        self.client = Client()
+
+    def test_get_user_profile_view(self):
+        """Test GET user profile"""
+        # Get profile as non logged-in user
+        profile_url = reverse('user_profile', kwargs={'username': 'foo'})
+        response = self.client.get(profile_url)
+        self.assertEqual(response.status_code, 403)
+        # Get your own profile as a logged-in user
+        self.client.login(username='foo', password='foobar')
+        response = self.client.get(profile_url)
+        self.assertEqual(response.status_code, 200)
+        # Get view profile of nonexistent profile
+        bar_profile_url = reverse('user_profile', kwargs={'username': 'bar'})
+        response = self.client.get(bar_profile_url)
+        self.assertEqual(response.status_code, 404)
+        # Get view profile of other user
+        user = User.objects.create_user(username='bar', password='foobar')
+        systersuser = SystersUser.objects.get(user=user)
+        response = self.client.get(bar_profile_url)
+        self.assertEqual(response.status_code, 403)
+        # Get view profile as superusers
+        self.user.is_superuser = True
+        self.user.save()
+        response = self.client.get(bar_profile_url)
+        self.assertEqual(response.status_code, 200)
+        # Get view profile of other user having the necessary permissions
+        self.user.is_superuser = True
+        self.user.save()
+        community = Community.objects.create(name="Foo", slug="foo", order=1,
+                                             community_admin=self.systers_user)
+        community.add_member(systersuser)
+        community.save()
+        response = self.client.get(bar_profile_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_user_profile_view(self):
+        """Test POST user profile"""
+        self.client.login(username='foo', password='foobar')
+        profile_url = reverse('user_profile', kwargs={'username': 'foo'})
+        response = self.client.post(profile_url, data={'first_name': 'Foo'})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.endswith('/users/foo/'))
+        user = User.objects.get(username='foo')
+        self.assertEqual(user.first_name, 'Foo')
+
+        bar_profile_url = reverse('user_profile', kwargs={'username': 'bar'})
+        response = self.client.post(bar_profile_url, data={})
+        self.assertEqual(response.status_code, 404)
+
+        self.user.is_superuser = True
+        self.user.save()
+        user = User.objects.create_user(username='bar', password='foobar')
+        SystersUser.objects.get(user=user)
+        response = self.client.post(bar_profile_url,
+                                    data={'first_name': 'Bar'})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.endswith('/users/bar/'))
+        user = User.objects.get(username='bar')
+        self.assertEqual(user.first_name, 'Bar')
+
+    def test_user_profile_view_templates(self):
+        """Test the usage of templates and content in UserProfileView"""
+        profile_url = reverse('user_profile', kwargs={'username': 'foo'})
+        self.client.login(username='foo', password='foobar')
+        response = self.client.get(profile_url)
+        self.assertTemplateUsed(response, 'users/edit_profile.html')
+        self.assertContains(response, "Edit my profile")
+        self.assertContains(response, "/users/foo/")
+
+        self.user.is_superuser = True
+        self.user.save()
+        User.objects.create_user(username='bar', password='foobar')
+        bar_profile_url = reverse('user_profile', kwargs={'username': 'bar'})
+        response = self.client.get(bar_profile_url)
+        self.assertContains(response, "Edit profile of bar")
+        self.assertContains(response, "/users/bar/")
+
+
 class UserTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='foo', password='foobar')
