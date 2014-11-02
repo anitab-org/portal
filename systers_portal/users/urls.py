825335afc3585ec8c8274892626c5e5265862e3b
ana-balica <ana.balica@gmail.com>
2014-11-02 14:34:13 +0800

    create UserProfileView + template + route

--- systers_portal/users/urls.py
+++ systers_portal/users/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from users.views import UserView
+from users.views import UserView, UserProfileView
 
 
 urlpatterns = [
-    url(r'(?P<username>[\w.@+-]+)/$', UserView.as_view(), name='user'),
+    url(r'^(?P<username>[\w.@+-]+)/$', UserView.as_view(), name='user'),
+    url(r'^(?P<username>[\w.@+-]+)/profile/$', UserProfileView.as_view(),
+        name='user_profile'),
 ]
--- systers_portal/users/views.py
+++ systers_portal/users/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User
 from django.views.generic import TemplateView
+from django.views.generic.edit import UpdateView
 from django.shortcuts import get_object_or_404
-from braces.views import LoginRequiredMixin
+from braces.views import LoginRequiredMixin, MultiplePermissionsRequiredMixin
 
 from community.models import JoinRequest
+from users.forms import UserForm
 from users.models import SystersUser
 
 
@@ -32,3 +35,54 @@ class UserView(LoginRequiredMixin, TemplateView):
         for key, value in context_dict.iteritems():
             context[key] = value
         return context
+
+
+class UserProfileView(LoginRequiredMixin, MultiplePermissionsRequiredMixin,
+                      UpdateView):
+    """User profile update view"""
+    template_name = "users/edit_profile.html"
+    form_class = UserForm
+    model = User
+    raise_exception = True
+    # TODO: add `redirect_unauthenticated_users = True` when django-braces will
+    # reach version 1.5
+
+    def dispatch(self, request, *args, **kwargs):
+        """Override dispatch method to add to the view User and SystersUser
+        objects, of which the profile is displayed."""
+        username = kwargs.get('username')
+        self.user = get_object_or_404(User, username=username)
+        self.systersuser = get_object_or_404(SystersUser, user=self.user)
+        return super(UserProfileView, self).dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        """Get form instance"""
+        return self.user
+
+    def get_context_data(self, **kwargs):
+        """Supply additional context data for the template"""
+        context = super(UserProfileView, self).get_context_data(**kwargs)
+        context['systersuser'] = self.systersuser
+        return context
+
+    def get_success_url(self):
+        """Supply the redirect URL in case of successful submit"""
+        return self.systersuser.get_absolute_url()
+
+    def check_permissions(self, request):
+        """Check if the request user has any of those permissions:
+
+        * is a superuser
+        * is the owner of the profile (can edit it's own profile)
+        * has the permission to change a community systersuser, if systersuser
+          is member of any of those communities
+        """
+        permissions = []
+        communities = self.systersuser.communities.all()
+        for community in communities:
+            permissions.extend([
+                request.user.has_perm("change_community_systersuser",
+                                      community)])
+        permissions.extend([request.user.is_superuser])
+        permissions.extend([request.user == self.user])
+        return any(permissions)
