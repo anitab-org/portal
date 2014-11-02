e36ea03ca19dd39507cec79b23d8454895dc5921
ana-balica <ana.balica@gmail.com>
2014-11-02 12:23:55 +0800

    add crispy form style to UserForm

--- systers_portal/users/forms.py
+++ systers_portal/users/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Div, HTML
 
 from users.models import SystersUser
 
@@ -21,6 +23,23 @@ class UserForm(forms.ModelForm):
 
         self.fields.update(self.systers_user_form.fields)
         self.initial.update(self.systers_user_form.initial)
+
+        # crispy FormHelper customization
+        self.helper = FormHelper(self)
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-3'
+        self.helper.field_class = 'col-lg-9'
+        self.helper.layout.append(
+            Div(
+                Div(
+                    HTML("""<a role="button" class="btn btn-default mr4"
+                            href="{{ systersuser.get_absolute_url }}">
+                            Cancel</a>"""),
+                    Submit('save', 'submit', css_class='btn btn-primary'),
+                    css_class='col-lg-9 col-lg-offset-3',
+                ),
+                css_class='form-group',
+            ))
 
     def save(self, *args, **kwargs):
         self.systers_user_form.save(*args, **kwargs)
