7bafc7cfdf09581f4632d9b8ad2e2b3534de8383
ana-balica <ana.balica@gmail.com>
2014-11-03 11:46:32 +0800

    fix User redirect urls

--- systers_portal/users/adapter.py
+++ systers_portal/users/adapter.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 class SystersUserAccountAdapter(DefaultAccountAdapter):
     """Custom account adapter with different than default redirect URLs"""
     def get_login_redirect_url(self, request):
-        return reverse('user_profile', args=[request.user.username])
+        return reverse('user', args=[request.user.username])
 
     def get_signup_redirect_url(self, request):
-        return reverse('user_profile', args=[request.user.username])
+        return reverse('user', args=[request.user.username])
