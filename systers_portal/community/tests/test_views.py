16ceb8cf814fbd959f19fe226afbb80ce9ec265d
ana-balica <ana.balica@gmail.com>
2014-12-05 19:46:08 +0800

    show community landing page and specific community page
    Includes:
    * routing
    * template
    * view
    * unittests

--- systers_portal/community/tests/test_views.py
+++ systers_portal/community/tests/test_views.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from django.db.models.signals import post_save, post_delete
 from django.test import TestCase, Client
 
-from community.models import Community
+from community.models import Community, CommunityPage, JoinRequest
 from community.signals import manage_community_groups, remove_community_groups
 from users.models import SystersUser
 
@@ -99,3 +99,107 @@ class EditCommunityProfileViewTestCase(TestCase):
         response = self.client.post(url, data=data)
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response.url.endswith('/community/bar/profile/'))
+
+
+class CommunityPageViewTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='foo', password='foobar')
+        self.systers_user = SystersUser.objects.get()
+        self.community = Community.objects.create(name="Foo", slug="foo",
+                                                  order=1,
+                                                  community_admin=self.
+                                                  systers_user)
+
+    def test_join_button_snippet(self):
+        """Test the rendering of join button snippet"""
+        url = reverse('view_community_main_page', kwargs={'slug': 'foo'})
+        response = self.client.get(url)
+        self.assertNotContains(response, "Join Community")
+
+        self.client.login(username='foo', password='foobar')
+        response = self.client.get(url)
+        self.assertContains(response, "Transfer ownership")
+
+        user = User.objects.create_user(username='bar', password='foobar')
+        systers_user_bar = SystersUser.objects.get(user=user)
+        self.client.login(username='bar', password='foobar')
+        response = self.client.get(url)
+        self.assertContains(response, "Join Community")
+
+        self.community.add_member(systers_user_bar)
+        self.community.save()
+        response = self.client.get(url)
+        self.assertContains(response, "Leave Community")
+
+        self.community.remove_member(systers_user_bar)
+        self.community.save()
+        join_request = JoinRequest.objects.create(user=systers_user_bar,
+                                                  community=self.community)
+        response = self.client.get(url)
+        self.assertContains(response, "Cancel request")
+
+        join_request.is_approved = True
+        join_request.save()
+        response = self.client.get(url)
+        self.assertContains(response, "Join Community")
+
+    def test_actions_sidebar_snippet(self):
+        """Test the rendering of actions sidebar snippet"""
+        url = reverse('view_community_main_page', kwargs={'slug': 'foo'})
+        response = self.client.get(url)
+        self.assertNotContains(response, "Community Actions")
+
+        User.objects.create_user(username='bar', password='foobar')
+        # systers_user_bar = SystersUser.objects.get(user=user)
+        self.client.login(username='bar', password='foobar')
+        response = self.client.get(url)
+        self.assertContains(response, "Community Actions")
+        self.assertContains(response, "View Community Profile")
+        self.assertNotContains(response, "Edit Community Profile")
+        self.assertNotContains(response, "Manage Community Pages")
+        self.assertNotContains(response, "Manage Community Users")
+        self.assertNotContains(response, "Show Join Requests")
+
+        self.client.login(username='foo', password='foobar')
+        response = self.client.get(url)
+        self.assertContains(response, "Community Actions")
+        self.assertContains(response, "View Community Profile")
+        self.assertContains(response, "Edit Community Profile")
+        self.assertContains(response, "Manage Community Pages")
+        self.assertContains(response, "Manage Community Users")
+        self.assertContains(response, "Show Join Requests")
+
+    def test_get_view_community_main_page(self):
+        """Test GET request to land on community main page, which is in fact
+        a community page view"""
+        url = reverse('view_community_main_page', kwargs={'slug': 'foo'})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'community/page.html')
+        self.assertEqual(response.context['active_page'], 'news')
+
+        CommunityPage.objects.create(slug="page", title="Page", order=1,
+                                     author=self.systers_user,
+                                     community=self.community)
+        response = self.client.get(url)
+        self.assertEqual(response.context['active_page'], 'page')
+
+    def test_get_community_page(self):
+        """Test GET request to view a community page"""
+        CommunityPage.objects.create(slug="page", title="Page", order=1,
+                                     author=self.systers_user,
+                                     community=self.community)
+        url = reverse('view_community_page', kwargs={'slug': 'foo',
+                                                     'page_slug': 'page'})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'community/page.html')
+        self.assertEqual(response.context['active_page'], 'page')
+
+        CommunityPage.objects.create(slug="page2", title="Page2", order=2,
+                                     author=self.systers_user,
+                                     community=self.community)
+        url = reverse('view_community_page', kwargs={'slug': 'foo',
+                                                     'page_slug': 'page2'})
+        response = self.client.get(url)
+        self.assertEqual(response.context['active_page'], 'page2')
--- systers_portal/community/views.py
+++ systers_portal/community/views.py
@@ -4,8 +4,10 @@ from django.views.generic import DetailView
 from django.views.generic.edit import UpdateView
 from braces.views import LoginRequiredMixin, PermissionRequiredMixin
 
+from community.constants import DEFAULT_COMMUNITY_ACTIVE_PAGE
 from community.forms import CommunityForm
-from community.models import Community
+from community.models import Community, CommunityPage
+from users.models import SystersUser
 
 
 class ViewCommunityProfileView(DetailView):
@@ -34,3 +36,54 @@ class EditCommunityProfileView(LoginRequiredMixin, PermissionRequiredMixin,
         profile. The permission holds true for superusers."""
         community = get_object_or_404(Community, slug=self.kwargs['slug'])
         return request.user.has_perm("change_community", community)
+
+
+class CommunityPageView(DetailView):
+    """Community page view"""
+    template_name = "community/page.html"
+    model = Community
+
+    def get_context_data(self, **kwargs):
+        """Supply additional context data, such as:
+
+        * SystersUser object
+        * Indicator if user is member of the community
+        * JoinRequest object if exists
+        * All community pages (CommunityPage objects)
+        * Current active page slug
+        """
+        context = super(CommunityPageView, self).get_context_data(**kwargs)
+        community = context['community']
+        user = self.request.user
+        if user.username:
+            systers_user = SystersUser.objects.get(user=user)
+            context['is_member'] = systers_user.is_member(community)
+            context['join_request'] = systers_user.get_last_join_request(
+                community)
+
+        extra_context = self.get_pages_context(community)
+        context.update(extra_context)
+        return context
+
+    def get_pages_context(self, community):
+        """Return extra context to the view, such as all community page and
+        current active page slug.
+
+        :param community: Community object
+        :return: dict with extra context
+        """
+        context = {}
+        pages = CommunityPage.objects.filter(community=community).\
+            order_by('order')
+        context['pages'] = pages
+
+        page_slug = self.kwargs.get('page_slug')
+        if page_slug:
+            page = get_object_or_404(CommunityPage, slug=page_slug)
+            context['active_page'] = page.slug
+        else:
+            if pages:
+                context['active_page'] = pages[0].slug
+            else:
+                context['active_page'] = DEFAULT_COMMUNITY_ACTIVE_PAGE
+        return context
