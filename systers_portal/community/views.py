13a61c94a31d90e60ff0b8fecc813f80646d801f
ana-balica <ana.balica@gmail.com>
2014-12-30 11:23:05 +0700

    use CommunityMenuMixin for CommunityPageView

--- systers_portal/community/views.py
+++ systers_portal/community/views.py
@@ -4,8 +4,8 @@ from django.views.generic import DetailView, ListView
 from django.views.generic.edit import UpdateView
 from braces.views import LoginRequiredMixin, PermissionRequiredMixin
 
-from community.constants import DEFAULT_COMMUNITY_ACTIVE_PAGE
 from community.forms import CommunityForm
+from community.mixins import CommunityMenuMixin
 from community.models import Community, CommunityPage
 from blog.models import News
 from users.models import SystersUser
@@ -39,7 +39,7 @@ class EditCommunityProfileView(LoginRequiredMixin, PermissionRequiredMixin,
         return request.user.has_perm("change_community", community)
 
 
-class CommunityPageView(DetailView):
+class CommunityPageView(CommunityMenuMixin, DetailView):
     """Community page view"""
     template_name = "community/base.html"
     model = Community
@@ -50,8 +50,6 @@ class CommunityPageView(DetailView):
         * SystersUser object
         * Indicator if user is member of the community
         * JoinRequest object if exists
-        * All community pages (CommunityPage objects)
-        * Current active page slug
         """
         context = super(CommunityPageView, self).get_context_data(**kwargs)
         community = context['community']
@@ -61,30 +59,26 @@ class CommunityPageView(DetailView):
             context['is_member'] = systers_user.is_member(community)
             context['join_request'] = systers_user.get_last_join_request(
                 community)
-
-        extra_context = self.get_pages_context(community)
-        context.update(extra_context)
         return context
 
-    def get_pages_context(self, community):
-        """Return extra context to the view, such as all community page and
-        current active page slug.
+    def get_community(self):
+        """Overrides the method from CommunityMenuMixin to extract the current
+        community.
 
-        :param community: Community object
-        :return: dict with extra context
+        :return: Community object
         """
-        context = {}
-        pages = CommunityPage.objects.filter(community=community).\
-            order_by('order')
-        context['pages'] = pages
+        community_slug = self.kwargs['slug']
+        return get_object_or_404(Community, slug=community_slug)
 
+    def get_page(self):
+        """Overrides the method from CommunityMenuMixin to extract the current
+        page or the lack of it.
+
+        :return: CommunityPage object if provided, otherwise None
+        """
         page_slug = self.kwargs.get('page_slug')
         if page_slug:
-            page = get_object_or_404(CommunityPage, slug=page_slug)
-            context['active_page'] = page.slug
-        else:
-            if pages:
-                context['active_page'] = pages[0].slug
-            else:
-                context['active_page'] = DEFAULT_COMMUNITY_ACTIVE_PAGE
+            return get_object_or_404(CommunityPage, slug=page_slug)
+        return None
+
         return context
