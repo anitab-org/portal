8b6f0ed0280811d4953f62537aa92b169bb1e2a9
ana-balica <ana.balica@gmail.com>
2015-01-29 23:03:22 +0700

    resource add form + unittest

--- systers_portal/blog/forms.py
+++ systers_portal/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from crispy_forms.helper import FormHelper
 
 from common.crispy_forms.bootstrap import SubmitCancelFormActions
-from blog.models import News
+from blog.models import News, Resource
 from users.models import SystersUser
 
 
@@ -58,3 +58,38 @@ class EditNewsForm(forms.ModelForm):
                 cancel_href="{% url 'view_community_news' community.slug "
                             "news.slug %}")
         )
+
+
+class AddResourceForm(forms.ModelForm):
+    """Form to add new Community Resource. The author and the community of the
+     rseource should be provided by the view:
+
+     * author - currently logged in user
+     * community - defined by the community slug from the URL
+     """
+    class Meta:
+        model = Resource
+        fields = ['slug', 'title', 'content', 'is_public', 'is_monitored',
+                  'tags', 'resource_type']
+
+    def __init__(self, *args, **kwargs):
+        self.author = kwargs.pop('author')
+        self.community = kwargs.pop('community')
+        super(AddResourceForm, self).__init__(*args, **kwargs)
+
+        # crispy FormHelper customization
+        self.helper = FormHelper(self)
+        self.helper.layout.append(
+            SubmitCancelFormActions(
+                cancel_href="{% url 'view_community_resource_list' "
+                            "community.slug %}")
+        )
+
+    def save(self, commit=True):
+        """Override save to add author and community to the instance."""
+        instance = super(AddResourceForm, self).save(commit=False)
+        instance.author = SystersUser.objects.get(user=self.author)
+        instance.community = self.community
+        if commit:
+            instance.save()
+        return instance
--- systers_portal/blog/tests/test_forms.py
+++ systers_portal/blog/tests/test_forms.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from blog.forms import AddNewsForm, EditNewsForm
-from blog.models import News
+from blog.forms import AddNewsForm, EditNewsForm, AddResourceForm
+from blog.models import News, Resource
 from community.models import Community
 from users.models import SystersUser
 
@@ -68,3 +68,33 @@ class EditNewsFormTestCase(TestCase):
         self.assertEqual(news.slug, 'bar')
         self.assertEqual(news.title, 'Bar')
         self.assertEqual(news.content, 'Rainbows and ponies')
+
+
+class AddResourceFormTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='foo', password='foobar')
+        self.systers_user = SystersUser.objects.get()
+        self.community = Community.objects.create(name="Foo", slug="foo",
+                                                  order=1,
+                                                  community_admin=self.
+                                                  systers_user)
+
+    def test_add_resource_form(self):
+        """Test add resource form"""
+        invalid_data = {'slug': "new"}
+        form = AddResourceForm(data=invalid_data, author=self.user,
+                               community=self.community)
+        self.assertFalse(form.is_valid())
+
+        data = {'slug': 'bar',
+                'title': 'Bar',
+                'content': "Rainbows and ponies",
+                'is_public': 1,
+                'is_monitored': 0}
+        form = AddResourceForm(data=data, author=self.user,
+                               community=self.community)
+        self.assertTrue(form.is_valid())
+        form.save()
+        resource = Resource.objects.get()
+        self.assertEqual(resource.title, 'Bar')
+        self.assertEqual(resource.author, self.systers_user)
