0633462c1ba64fc8855dc9b3a388f0f26a809583
ana-balica <ana.balica@gmail.com>
2015-01-30 18:26:10 +0700

    add community page + unittest

--- systers_portal/community/forms.py
+++ systers_portal/community/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from crispy_forms.helper import FormHelper
 
 from common.crispy_forms.bootstrap import SubmitCancelFormActions
-from community.models import Community
+from community.models import Community, CommunityPage
+from users.models import SystersUser
 
 
 class CommunityForm(forms.ModelForm):
@@ -22,3 +23,37 @@ class CommunityForm(forms.ModelForm):
                 cancel_href="{% url 'view_community_profile' "
                             "community.slug %}")
         )
+
+
+class AddCommunityPageForm(forms.ModelForm):
+    """Form to create new CommunityPage. The author and the community of the
+    page are expected to be provided when initializing the form:
+
+    * author - currently logged in user, aka the author of the page
+    * community - to which Community the CommunityPage belongs
+    """
+    class Meta:
+        model = CommunityPage
+        fields = ('slug', 'title', 'order', 'content')
+
+    def __init__(self, *args, **kwargs):
+        self.author = kwargs.pop('author')
+        self.community = kwargs.pop('community')
+        super(AddCommunityPageForm, self).__init__(*args, **kwargs)
+
+        # crispy FormHelper customization
+        self.helper = FormHelper(self)
+        self.helper.layout.append(
+            SubmitCancelFormActions(
+                cancel_href="{% url 'view_community_main_page' "
+                            "community.slug %}")
+        )
+
+    def save(self, commit=True):
+        """Override save to add author and community to the instance"""
+        instance = super(AddCommunityPageForm, self).save(commit=False)
+        instance.author = SystersUser.objects.get(user=self.author)
+        instance.community = self.community
+        if commit:
+            instance.save()
+        return instance
--- systers_portal/community/tests/test_forms.py
+++ systers_portal/community/tests/test_forms.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from community.forms import CommunityForm
-from community.models import Community
+from community.forms import CommunityForm, AddCommunityPageForm
+from community.models import Community, CommunityPage
 from users.models import SystersUser
 
 
@@ -26,3 +26,32 @@ class CommunityFormTestCase(TestCase):
         form.save()
         self.assertEqual(self.community.name, 'Bar')
         self.assertEqual(self.community.slug, 'bar')
+
+
+class AddCommunityPageFormTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='foo', password='foobar')
+        self.systers_user = SystersUser.objects.get()
+        self.community = Community.objects.create(name="Foo", slug="foo",
+                                                  order=1,
+                                                  community_admin=self.
+                                                  systers_user)
+
+    def test_add_community_page_form(self):
+        """Test add CommunityPage form"""
+        invalid_data = {'title': "something"}
+        form = AddCommunityPageForm(data=invalid_data, author=self.user,
+                                    community=self.community)
+        self.assertFalse(form.is_valid())
+
+        data = {'slug': 'foo',
+                'title': 'Foo',
+                'order': 1,
+                'content': "Rainbows and ponies"}
+        form = AddCommunityPageForm(data=data, author=self.user,
+                                    community=self.community)
+        self.assertTrue(form.is_valid())
+        form.save()
+        resource = CommunityPage.objects.get()
+        self.assertEqual(resource.title, 'Foo')
+        self.assertEqual(resource.author, self.systers_user)
