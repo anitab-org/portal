b492963afd62e9824065eaf7cd1379bb5a8e834e
ana-balica <ana.balica@gmail.com>
2015-02-11 18:53:47 +0700

    create a custom ModelFormWithHelper + tests

---
+++ systers_portal/common/forms.py
@@ -0,0 +1,39 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.forms import ModelForm
+
+
+class ModelFormWithHelper(ModelForm):
+    """Custom ModelForm that allows to attach a crispy-forms FormHelper class,
+    that will modify in some way the rendering of the layout.
+
+    Example::
+
+        FooForm(ModelFormWithHelper):
+            class Meta:
+                model = FooModel
+                helper_class = FooFormHelper
+    """
+    def __init__(self, *args, **kwargs):
+        super(ModelFormWithHelper, self).__init__(*args, **kwargs)
+
+        if hasattr(self.Meta, "helper_class"):
+            helper_class = getattr(self.Meta, "helper_class")
+            kwargs = self.get_helper_kwargs()
+            self.helper = helper_class(self, **kwargs)
+        else:
+            raise ImproperlyConfigured(
+                "{0} is missing a 'helper_class' meta attribute.".format(
+                    self.__class__.__name__))
+
+    def get_helper_kwargs(self):
+        """Get all helper attributes from class Meta by stripping them of
+        `helper_` part of attribute string
+
+        :return: dict with helper kwargs
+        """
+        kwargs = {}
+        for attr, value in self.Meta.__dict__.iteritems():
+            if attr.startswith("helper_") and attr != "helper_class":
+                new_attr = attr.split("_", 1)[1]
+                kwargs[new_attr] = value
+        return kwargs
---
+++ systers_portal/common/tests/test_forms.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ImproperlyConfigured
+from django.test import TestCase
+from crispy_forms.helper import FormHelper
+
+from common.forms import ModelFormWithHelper
+from common.helpers import SubmitCancelFormHelper
+
+
+class ModelFormWithHelperTestCase(TestCase):
+    def test_model_form_without_helper(self):
+        """Test the custom model form without a helper class"""
+        class FooForm(ModelFormWithHelper):
+            class Meta:
+                model = User
+
+        self.assertRaises(ImproperlyConfigured, FooForm, {})
+
+    def test_model_form_with_helper(self):
+        """Test the custom model form with a helper class present"""
+        class FooForm(ModelFormWithHelper):
+            class Meta:
+                model = User
+                helper_class = FormHelper
+
+        form = FooForm()
+        self.assertEqual(form.helper.__class__, FormHelper)
+
+        class BarForm(ModelFormWithHelper):
+            class Meta:
+                model = User
+                helper_class = SubmitCancelFormHelper
+                helper_cancel_href = "some/url/"
+
+        form = BarForm()
+        self.assertEqual(form.helper.__class__, SubmitCancelFormHelper)
