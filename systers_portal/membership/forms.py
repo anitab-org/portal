3efe6ae8b8a77007c18bd980e0281a2f9814f342
ana-balica <ana.balica@gmail.com>
2015-02-13 17:24:34 +0700

    form for transfering ownership of a community

---
+++ systers_portal/membership/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from common.helpers import SubmitCancelFormHelper
+
+
+class TransferOwnershipForm(forms.Form):
+    """Form with a single field that allow a single choice out of a list of
+    members of a community. Used to select the new admin of a community."""
+    def __init__(self, *args, **kwargs):
+        self.community = kwargs.pop('community')
+        super(TransferOwnershipForm, self).__init__(*args, **kwargs)
+        members = self.community.members.all()
+        members = members.exclude(pk=self.community.community_admin.pk)
+        choices = [(member.id, str(member)) for member in members]
+        self.fields['new_admin'] = forms.ChoiceField(
+            choices=choices, label="New community admin")
+
+        self.helper = SubmitCancelFormHelper(
+            self, cancel_href="{% url 'user' user.username %}")
---
+++ systers_portal/membership/tests/test_forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from community.models import Community
+from membership.forms import TransferOwnershipForm
+from users.models import SystersUser
+
+
+class TransferOwnershipFormTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='foo', password='foobar')
+        self.systers_user = SystersUser.objects.get()
+        self.community = Community.objects.create(name="Foo", slug="foo",
+                                                  order=1,
+                                                  community_admin=self.
+                                                  systers_user)
+
+    def test_transfer_ownership_form(self):
+        """Test transferring ownership form"""
+        form = TransferOwnershipForm(community=self.community)
+        self.assertIsInstance(form.fields['new_admin'], forms.ChoiceField)
+        self.assertSequenceEqual(form.fields['new_admin'].choices, [])
+
+        bar_user = User.objects.create_user(username="bar", password="foobar")
+        bar_systers_user = SystersUser.objects.get(user=bar_user)
+        self.community.add_member(bar_systers_user)
+
+        User.objects.create_user(username="new", password="foobar")
+
+        form = TransferOwnershipForm(community=self.community)
+        self.assertSequenceEqual(form.fields['new_admin'].choices,
+                                 [(bar_user.id, "bar")])
