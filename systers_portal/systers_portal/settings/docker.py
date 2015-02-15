5b4200e2566d200b358c7f3f2914030aa5871695
ana-balica <ana.balica@gmail.com>
2015-02-15 20:09:13 +0700

    fig.yml and a docker specific settings file (for postgres)

---
+++ systers_portal/systers_portal/settings/docker.py
@@ -0,0 +1,21 @@
+from base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+# Instead of sending out real email, during development the emails will be sent
+# to stdout, where from they can be inspected.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
