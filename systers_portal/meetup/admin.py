15ad55278f56df40472bba36c5866eb5c4950717
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-06-22 18:21:54 +0530

    meetup model registered to admin

--- systers_portal/meetup/admin.py
+++ systers_portal/meetup/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from meetup.models import MeetupLocation
+from meetup.models import MeetupLocation, Meetup
 
 
 admin.site.register(MeetupLocation)
+admin.site.register(Meetup)
