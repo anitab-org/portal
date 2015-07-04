8bfef0147110830490cb5d9d4faaad84534ea0aa
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-07-04 14:34:55 +0530

    Meetup Location AboutView using mixin to fetch MeetupLocation instance.

--- systers_portal/meetup/views.py
+++ systers_portal/meetup/views.py
@@ -1,9 +1,14 @@
-from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView
 
+from meetup.mixins import MeetupLocationMixin
 from meetup.models import MeetupLocation
 
 
-class MeetupLocationAboutView(DetailView):
+class MeetupLocationAboutView(MeetupLocationMixin, TemplateView):
     """Meetup Location about view, show about description of Meetup Location"""
     model = MeetupLocation
     template_name = "meetup/about.html"
+
+    def get_meetup_location(self):
+        return get_object_or_404(MeetupLocation, slug=self.kwargs['slug'])
