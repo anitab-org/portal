957cfee51fd1025dfc3bad5f777c985b2924c61a
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-07-04 18:21:07 +0530

    Test for meetup location mixins

---
+++ systers_portal/meetup/tests/test_mixins.py
@@ -0,0 +1,34 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.test import TestCase
+from django.views.generic import TemplateView
+from cities_light.models import City, Country
+
+from meetup.mixins import MeetupLocationMixin
+from meetup.models import MeetupLocation
+
+
+class MeetupLocationMixinTestCase(TestCase):
+    def setUp(self):
+        country = Country.objects.create(name='Bar', continent='AS')
+        self.location = City.objects.create(name='Foo', display_name='Foo', country=country)
+        self.meetup_location = MeetupLocation.objects.create(
+            name="Foo Systers", slug="foo", location=self.location,
+            description="It's a test location")
+
+    def test_get_context_data_no_meetup_location(self):
+        """Test mixin with no meetup_location"""
+        class DummyView(MeetupLocationMixin, TemplateView):
+            pass
+
+        view = DummyView()
+        self.assertRaises(ImproperlyConfigured, view.get_context_data)
+
+    def test_get_context_data_with_meeetup_location(self):
+        """Test mixin with a meetup_location"""
+        class DummyView(MeetupLocationMixin, TemplateView):
+            def get_meetup_location(self):
+                return MeetupLocation.objects.get()
+
+        view = DummyView()
+        context = view.get_context_data()
+        self.assertEqual(context['meetup_location'], self.meetup_location)
