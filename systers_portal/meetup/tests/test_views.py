b51b1cdd0d978ddc9089f71f5f2cc93735bb8e3a
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-07-12 01:08:33 +0530

    Meetup Page view test

--- systers_portal/meetup/tests/test_views.py
+++ systers_portal/meetup/tests/test_views.py
@@ -1,12 +1,17 @@
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.utils import timezone
 from cities_light.models import City, Country
 
-from meetup.models import MeetupLocation
+from meetup.models import Meetup, MeetupLocation
+from users.models import SystersUser
 
 
 class MeetupLocationViewBaseTestCase(object):
     def setUp(self):
+        self.user = User.objects.create_user(username='foo', password='foobar')
+        self.systers_user = SystersUser.objects.get()
         country = Country.objects.create(name='Bar', continent='AS')
         self.location = City.objects.create(name='Baz', display_name='Baz', country=country)
         self.meetup_location = MeetupLocation.objects.create(
@@ -50,3 +55,37 @@ class MeetupLocationListViewTestCase(MeetupLocationViewBaseTestCase, TestCase):
         self.assertContains(response, "Bar Systers")
         self.assertEqual(len(response.context['object_list']), 2)
         self.assertContains(response, "google.maps.Map")
+
+
+class MeetupViewTestCase(MeetupLocationViewBaseTestCase, TestCase):
+    def setUp(self):
+        super(MeetupViewTestCase, self).setUp()
+        self.meetup = Meetup.objects.create(title='Foo Bar Baz', slug='foo-bar-baz',
+                                            date=timezone.now().date(),
+                                            time=timezone.now().time(),
+                                            description='This is test Meetup',
+                                            meetup_location=self.meetup_location,
+                                            created_by=self.systers_user,
+                                            last_updated=timezone.now())
+
+    def test_view_meetup(self):
+        """Test Meetup view for correct response"""
+        url = reverse('view_meetup', kwargs={'slug': 'foo', 'meetup_slug': 'foo-bar-baz'})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'meetup/meetup.html')
+        self.assertEqual(response.context['meetup_location'], self.meetup_location)
+        self.assertEqual(response.context['meetup'], self.meetup)
+
+        nonexistent_url = reverse('view_meetup', kwargs={'slug': 'foo1',
+                                  'meetup_slug': 'bazbar'})
+        response = self.client.get(nonexistent_url)
+        self.assertEqual(response.status_code, 404)
+
+        self.meetup_location2 = MeetupLocation.objects.create(
+            name="Bar Systers", slug="bar", location=self.location,
+            description="It's a test meetup location")
+        incorrect_pair_url = reverse('view_meetup', kwargs={'slug': 'bar',
+                                     'meetup_slug': 'foo-bar-baz'})
+        response = self.client.get(incorrect_pair_url)
+        self.assertEqual(response.status_code, 404)
