272fe49be0365148462cc1f4c39d59b0693ae808
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-07-11 23:45:04 +0530

    Meetup Page url

--- systers_portal/meetup/urls.py
+++ systers_portal/meetup/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from meetup.views import MeetupLocationAboutView, MeetupLocationList
+from meetup.views import MeetupLocationAboutView, MeetupLocationList, MeetupView
 
 
 urlpatterns = [
     url(r'^(?P<slug>\w+)/about/$', MeetupLocationAboutView.as_view(),
         name='about_meetup_location'),
     url(r'locations/$', MeetupLocationList.as_view(), name='list_meetup_location'),
+    url(r'^(?P<slug>[\w-]+)/(?P<meetup_slug>[\w-]+)/$', MeetupView.as_view(), name="view_meetup"),
 ]
