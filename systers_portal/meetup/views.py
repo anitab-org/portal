f3b54bbba2ffb9056012c7a187d426bcc4e47cad
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-07-14 14:22:27 +0530

    MeetupLocation list view

--- systers_portal/meetup/views.py
+++ systers_portal/meetup/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
 
 from meetup.mixins import MeetupLocationMixin
 from meetup.models import MeetupLocation
@@ -12,3 +13,9 @@ class MeetupLocationAboutView(MeetupLocationMixin, TemplateView):
 
     def get_meetup_location(self):
         return get_object_or_404(MeetupLocation, slug=self.kwargs['slug'])
+
+
+class MeetupLocationList(ListView):
+    template_name = "meetup/list_location.html"
+    model = MeetupLocation
+    paginate_by = 20
