c194fd38e3e7ad1ab6758951d04359e2f7ff88a0
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-08-03 03:16:38 +0530

    Add Meetup url

--- systers_portal/meetup/urls.py
+++ systers_portal/meetup/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from meetup.views import (MeetupLocationAboutView, MeetupLocationList, MeetupView,
-                          MeetupLocationMembersView)
+                          MeetupLocationMembersView, AddMeetupView)
 
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
         name='about_meetup_location'),
     url(r'^(?P<slug>\w+)/members/$', MeetupLocationMembersView.as_view(),
         name='members_meetup_location'),
+    url(r'^(?P<slug>\w+)/add/$', AddMeetupView.as_view(), name='add_meetup'),
     url(r'locations/$', MeetupLocationList.as_view(), name='list_meetup_location'),
     url(r'^(?P<slug>[\w-]+)/(?P<meetup_slug>[\w-]+)/$', MeetupView.as_view(), name="view_meetup"),
 ]
