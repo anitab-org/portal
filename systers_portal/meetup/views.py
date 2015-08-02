fc716d2b894c66c4bde2ab82c7035710e02afbf4
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-08-03 03:17:43 +0530

    Add Meetup view

--- systers_portal/meetup/views.py
+++ systers_portal/meetup/views.py
@@ -1,8 +1,12 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
+from braces.views import LoginRequiredMixin
 
+from meetup.forms import AddMeetupForm
 from meetup.mixins import MeetupLocationMixin
 from meetup.models import Meetup, MeetupLocation
 
@@ -44,3 +48,29 @@ class MeetupLocationMembersView(MeetupLocationMixin, DetailView):
 
     def get_meetup_location(self):
         return get_object_or_404(MeetupLocation, slug=self.kwargs['slug'])
+
+
+class AddMeetupView(LoginRequiredMixin, MeetupLocationMixin, CreateView):
+    """Add new meetup"""
+    template_name = "meetup/add_meetup.html"
+    model = Meetup
+    form_class = AddMeetupForm
+    raise_exception = True
+
+    def get_success_url(self):
+        """Supply the redirect URL in case of successful submit"""
+        return reverse("view_meetup", kwargs={"slug": self.meetup_location.slug,
+                                              "meetup_slug": self.object.slug})
+
+    def get_form_kwargs(self):
+        """Add request user and meetup location object to the form kwargs.
+        Used to autofill form fields with created_by and meetup_location without
+        explicitly filling them up in the form."""
+        kwargs = super(AddMeetupView, self).get_form_kwargs()
+        self.meetup_location = get_object_or_404(MeetupLocation, slug=self.kwargs['slug'])
+        kwargs.update({'created_by': self.request.user})
+        kwargs.update({'meetup_location': self.meetup_location})
+        return kwargs
+
+    def get_meetup_location(self):
+        return self.meetup_location
