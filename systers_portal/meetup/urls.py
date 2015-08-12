2b217168f9663da4bcea43234deae6ebb97d010a
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-08-12 19:24:36 +0530

    url for delete meetup

--- systers_portal/meetup/urls.py
+++ systers_portal/meetup/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from meetup.views import (MeetupLocationAboutView, MeetupLocationList, MeetupView,
-                          MeetupLocationMembersView, AddMeetupView)
+                          MeetupLocationMembersView, AddMeetupView, DeleteMeetupView)
 
 
 urlpatterns = [
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r'^(?P<slug>[\w-]+)/members/$', MeetupLocationMembersView.as_view(),
         name='members_meetup_location'),
     url(r'^(?P<slug>[\w-]+)/add/$', AddMeetupView.as_view(), name='add_meetup'),
+    url(r'^(?P<slug>[\w-]+)/(?P<meetup_slug>[\w-]+)/delete/$', DeleteMeetupView.as_view(),
+        name='delete_meetup'),
     url(r'locations/$', MeetupLocationList.as_view(), name='list_meetup_location'),
     url(r'^(?P<slug>[\w-]+)/(?P<meetup_slug>[\w-]+)/$', MeetupView.as_view(), name="view_meetup"),
 ]
