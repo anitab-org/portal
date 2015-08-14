c8103a65504c99837fd68765a35817b824040852
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-08-15 01:46:28 +0530

    Modified url patterns to accept "-".

--- systers_portal/meetup/urls.py
+++ systers_portal/meetup/urls.py
@@ -5,11 +5,11 @@ from meetup.views import (MeetupLocationAboutView, MeetupLocationList, MeetupVie
 
 
 urlpatterns = [
-    url(r'^(?P<slug>\w+)/about/$', MeetupLocationAboutView.as_view(),
+    url(r'^(?P<slug>[\w-]+)/about/$', MeetupLocationAboutView.as_view(),
         name='about_meetup_location'),
-    url(r'^(?P<slug>\w+)/members/$', MeetupLocationMembersView.as_view(),
+    url(r'^(?P<slug>[\w-]+)/members/$', MeetupLocationMembersView.as_view(),
         name='members_meetup_location'),
-    url(r'^(?P<slug>\w+)/add/$', AddMeetupView.as_view(), name='add_meetup'),
+    url(r'^(?P<slug>[\w-]+)/add/$', AddMeetupView.as_view(), name='add_meetup'),
     url(r'locations/$', MeetupLocationList.as_view(), name='list_meetup_location'),
     url(r'^(?P<slug>[\w-]+)/(?P<meetup_slug>[\w-]+)/$', MeetupView.as_view(), name="view_meetup"),
 ]
