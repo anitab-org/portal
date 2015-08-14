1c780155ae651b8bfa8b0aa9e29a1c0292642cdd
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-08-14 13:51:07 +0530

    Rsvp model registered to admin

--- systers_portal/meetup/admin.py
+++ systers_portal/meetup/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from meetup.models import MeetupLocation, Meetup
+from meetup.models import MeetupLocation, Meetup, Rsvp
 
 
 admin.site.register(MeetupLocation)
 admin.site.register(Meetup)
+admin.site.register(Rsvp)
