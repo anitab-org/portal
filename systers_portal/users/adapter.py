anitab-org/portal

commit a0671e0b9bb75905cb14506c6ddb18b1ab70d65b

Show all necessary check for password strength

systers_portal/users/adapter.py
@@ -23,34 +23,9 @@ class SystersUserAccountAdapter(DefaultAccountAdapter):
             if digit and uppercase and special_char:
                 return password
 
-            elif not digit and not uppercase and special_char:
-                raise ValidationError(
-                    "Password must have atleast one uppercase letter and one digit.")
-
-            elif not special_char and not digit and uppercase:
-                raise ValidationError(
-                    "Password must have atleast one special character and one digit.")
-
-            elif not special_char and not uppercase and digit:
-                raise ValidationError(
-                    "Password must have atleast one uppercase letter and one special character.")
-
-            elif not special_char and digit and uppercase:
-                raise ValidationError(
-                    "Password must have atleast one special character")
-
-            elif not uppercase and digit and special_char:
-                raise ValidationError(
-                    "Password must have atleast one uppercase letter")
-
-            elif not digit and special_char and uppercase:
-                raise ValidationError("Password must have atleast one digit")
-
             else:
                 raise ValidationError(
-                    "Password must have atleast one uppercase letter," +
-                    " one special character and one digit")
-
+                    "Password must have at least, 6 characters, one uppercase letter, one special character and one digit.")          
         else:
             raise ValidationError("Password must have atleast 6 characters")
 

commit a2c90a82c6e77ba8a18263dd2227f2c97396fc0b

Always provide all password requirements to user.
Before, if user entered password shorter than 6 chars they still weren't provided with full requirements.

 Link: https://github.com/systers/portal/issues/247
 Fixes: a0671e0b9bb7 ("Show all necessary check for password strength")

systers_portal/users/adapter.py
@@ -19,16 +19,12 @@ class SystersUserAccountAdapter(DefaultAccountAdapter):
         digit = re.match(z, password)
         special_char = set(y).intersection(password)
         uppercase = re.match(x, password)
-        if len(password) >= 6:
-            if digit and uppercase and special_char:
-                return password
-
-            else:
-                raise ValidationError(
-                    "Password must have at least, 6 characters, one uppercase letter, "
-                    "one special character and one digit.")
+        if len(password) >= 6 and digit and uppercase and special_char:
+            return password
         else:
-            raise ValidationError("Password must have atleast 6 characters")
+            raise ValidationError(
+                "Password must have at least, 6 characters, one uppercase letter, "
+                "one special character and one digit.")
 
     def get_login_redirect_url(self, request):
         return reverse('user', args=[request.user.username])