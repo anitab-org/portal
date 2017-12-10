a16533079c9ae27feba3c1f6cb00d343b054caff
payal97 <payalmail@aol.in>
2017-12-11 00:35:32 +0530

    fixed support request templates and pep8 warnings

--- systers_portal/meetup/forms.py
+++ systers_portal/meetup/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from django.utils import timezone
 
 from common.forms import ModelFormWithHelper
 from common.helpers import SubmitCancelFormHelper
