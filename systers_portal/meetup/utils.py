3b23aa04ea0882c454d1aa074d265fae2682abc4
RitwickRaj78 <ritwickraj78@gmail.com>
2020-06-21 02:45:43 +0530

    Changed Meetup utils.py

--- systers_portal/meetup/utils.py
+++ systers_portal/meetup/utils.py
@@ -6,53 +6,46 @@ from meetup.permissions import groups_templates, group_permissions
 
 
 @transaction.atomic
-def create_groups(meetup_location):
+def create_groups(meetup):
     """Create groups for a Meetup Location instance using its name
 
+    :param meetup:
     :param meetup_location: string name of meetup location
     :return: list of meetup location Group objects
     """
-    meetup_location_groups = []
+    meetup_groups = []
     for key, group_name in groups_templates.items():
         group, created = Group.objects.get_or_create(
-            name=group_name.format(meetup_location))
-        meetup_location_groups.append(group)
-    return meetup_location_groups
+            name=group_name.format(meetup))
+        meetup_groups.append(group)
+    return meetup_groups
 
 
 @transaction.atomic
-def remove_groups(meetup_location):
+def remove_groups(meetup):
     """Remove groups for a particular Meetup Location instance using its name
-
-    :param meetup_location: string name of meetup location
     """
-    name = "{0}:".format(meetup_location)
+    name = "{0}:".format(meetup)
     Group.objects.filter(name__startswith=name).delete()
 
 
-def get_groups(meetup_location):
+def get_groups(meetup):
     """Get groups of a particular Meetup Location instance using its name
 
-    :param meetup_location: string name of Meetup Location
+    :param meetup:
     :return: list of Group objects
     """
-    name = "{0}:".format(meetup_location)
+    name = "{0}:".format(meetup)
     return Group.objects.filter(name__startswith=name)
 
 
-def assign_permissions(meetup_location, groups):
+def assign_permissions(meetup, groups):
     """Assign row-level permissions to meetup location groups and meetup location object
-
-    :param meetup_location: Meetup Location object
     :param groups: list of Group objects
     """
     for key, group_name in groups_templates.items():
         group = next(
-            g for g in groups if g.name == group_name.format(meetup_location.name))
+            g for g in groups if g.name == group_name.format(meetup.title))
         for perm in group_permissions[key]:
-            if (perm.endswith('meetup') or perm.endswith('meetuplocation') or
-                    perm.endswith('supportrequest')):
-                group.permissions.add(Permission.objects.get(codename=perm))
-                group.save()
-            else:
-                assign_perm(perm, group, meetup_location)
+            group.permissions.add(Permission.objects.get(codename=perm))
+            group.save()
