bf3c9fa76cb633c1a9622612e99da7064768ddcf
RitwickRaj78 <ritwickraj78@gmail.com>
2020-06-21 02:37:11 +0530

    Changed Meetup admin.py

--- systers_portal/meetup/admin.py
+++ systers_portal/meetup/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 
-from meetup.models import (Meetup, MeetupLocation, Rsvp, SupportRequest, RequestMeetupLocation,
-                           RequestMeetup)
+from meetup.models import (Meetup, Rsvp, SupportRequest, RequestMeetup)
 
-
-admin.site.register(MeetupLocation)
 admin.site.register(Meetup)
 admin.site.register(Rsvp)
 admin.site.register(SupportRequest)
 admin.site.register(RequestMeetup)
-admin.site.register(RequestMeetupLocation)
